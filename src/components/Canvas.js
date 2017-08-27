24a8bccb56147e9add89fd3b62c8ba42761951fe
byteme980 <byteme980@gmail.com>
2017-08-27 06:52:58 -0700

    style hanges

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -6,16 +6,23 @@ class Canvas extends Component {
   constructor() {
     super();
     this.paintWithColor = this.paintWithColor.bind(this);
+    this.startPainting = this.startPainting.bind(this);
+    this.stopPainting = this.stopPainting.bind(this);
   }
   paintWithColor(row, col) {
-    console.log('in canvas paint w color')
     const {onSquarePaint, color, mode} = this.props;
     onSquarePaint(row, col, color, mode);
+  }
+  startPainting(e) {
+    this.props.startPainting();
+  }
+  stopPainting(e) {
+    this.props.stopPainting();
   }
   render() {
     let {colorGrid, isPainting, startPainting, stopPainting, mode} = this.props;
     let cells = colorGrid.map((row, r) => (
-      <div className='canvas-row' key={'row-' + r} onTouchStart={startPainting} onTouchEnd={stopPainting} onMouseDown={startPainting} onMouseUp={stopPainting}>
+      <div className='canvas-row' key={'row-' + r} onMouseDown={this.startPainting} onMouseUp={this.stopPainting}>
         {
           row.map((col, c) => (
             <Cell color={colorGrid[r][c]} actionOnHover={isPainting} onHover={this.paintWithColor} onClick={this.paintWithColor} key={r + ',' + c}  row={r} col={c}/>
@@ -25,7 +32,7 @@ class Canvas extends Component {
     ))
 
     return (
-      <div className={'canvas-container ' + mode}>
+      <div className={'canvas-container ' + mode} >
         {cells}
       </div>
     )
